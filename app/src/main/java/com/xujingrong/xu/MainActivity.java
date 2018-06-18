13840dcdbe30083bba462c7d5d9033e716e04215
a2275426161@163.com <a2275426161@163.com>
2018-06-18 18:59:12 +0800

    h

--- app/src/main/java/com/xujingrong/xu/MainActivity.java
+++ app/src/main/java/com/xujingrong/xu/MainActivity.java
@@ -1,14 +1,32 @@
 package com.xujingrong.xu;
 
-import android.app.*;
-import android.os.*;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.media.MediaPlayer;
 
-public class MainActivity extends Activity 
+
+public class MainActivity extends AppCompatActivity
 {
+	private MediaPlayer mediaPlayer=new MediaPlayer();
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.main1);
+		mediaPlayer=MediaPlayer.create(this,R.raw.m);
+		if (!mediaPlayer.isPlaying()) {
+			mediaPlayer.start();
+		}
     }
+
+	@Override
+	protected void onDestroy()
+	{
+		super.onDestroy();
+		if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+        }
+	}
+	
 }
